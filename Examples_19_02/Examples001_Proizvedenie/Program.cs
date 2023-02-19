16b090edfe80320e6b0bb01a36c1b4e14317aaf7
Julyidol <lineoff@mail.com>
2023-02-19 14:54:36 +0300

    Добавили произведение чисел

---
+++ Examples_19_02/Examples001_Proizvedenie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ЗАДАЧА, выводящая произведение от 1 до N");
+int Multiply (int num){
+    int multiply = 1;
+    for( int i = 1; i <= num; i++){
+        multiply*=i;
+    }
+    return multiply;
+}
+Console.Write("Введите число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+int result2 = Multiply(number2);
+Console.WriteLine($"Произведение от 1 до {number2} = {result2}");
