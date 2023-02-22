ad1546ed4508f17027baa01eee1424af8e2da49a
Julyidol <lineoff@mail.com>
2023-02-22 20:25:18 +0300

    Добавили вывод третьего числа из 3х

---
+++ Examples_22_02/Examp_Lastcount/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("третья цифра этого числа -> "+stringNumber[2]);
